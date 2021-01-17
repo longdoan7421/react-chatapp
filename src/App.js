451946fca0ae1bfdab17d261eeee65985dfe1f8a
longdoan7421 <hoanglong7421@gmail.com>
2021-01-17 15:23:43 +0100

    Fix some imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ChatContainer from './Containers/ChatContainer';
-import ContactContainer from './Containers/ContactContainer';
+import Chat from './Components/Chat';
+import Contact from './Components/Contact';
 import './App.css';
 
 const App = () => {
   return (
     <div className="container-fluid h-100">
       <div className="row justify-content-center h-100">
-        <ContactContainer />
-        <ChatContainer />
+        <Chat />
+        <Contact />
       </div>
     </div>
   );
